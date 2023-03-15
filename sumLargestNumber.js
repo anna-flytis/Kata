08d629b6ab3ee969eeee88e5c2face2b0abbc0f1
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-03-15 12:21:01 -0600

    Sum largest numbers
    In this exercise, we will be given an array of 2 or more numbers. We will then have to find the two largest numbers in that array, and sum them together.

---
+++ sumLargestNumber.js
@@ -0,0 +1,31 @@
+const sumLargestNumbers = array => {
+  let largestNumbers = [];
+  let modifiedArray1 = [];
+  let modifiedArray2 = [];
+
+  if (array.length === 2) {
+    largestNumbers.push(array[0]);
+    largestNumbers.push(array[1]);
+    return largestNumbers[0] + largestNumbers[1];
+  }
+
+  function largeNumberSearcher(elements, garbageArray, goodArray) {
+    let largestNumberSoFar = elements[0];
+    for (index = 1; index < elements.length; index ++) {
+      if (elements[index] > largestNumberSoFar) {
+        garbageArray.push(largestNumberSoFar);
+        largestNumberSoFar = elements[index];
+      } else {
+        garbageArray.push(elements[index]);
+      }
+    }
+    goodArray.push(largestNumberSoFar);
+  }
+  largeNumberSearcher(array, modifiedArray1, largestNumbers)
+  largeNumberSearcher(modifiedArray1, modifiedArray2, largestNumbers);
+  return (largestNumbers[0] + largestNumbers[1]);
+}
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
